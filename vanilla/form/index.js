da8eafb86df29ad0c342ef1921a6937fc74fe4e9
Azamat Nigmatullin <ngmazamat@gmail.com>
2024-12-19 17:12:34 +0400

    comiit

--- vanilla/form/index.js
+++ vanilla/form/index.js
@@ -1,4 +1,6 @@
 function onClick() {
+  let zIndex = document.getElementById("z-index2");
+  let color = document.getElementById("color");
   let position = document.getElementById("position");
   let button = document.getElementById("button");
   let inputTop = document.getElementById("top");
@@ -12,9 +14,13 @@ function onClick() {
   block.style.left = inputLeft.value + unitOfMeasurement.value;
   block.style.right = inputRight.value + unitOfMeasurement.value;
   block.style.bottom = inputBottom.value + unitOfMeasurement.value;
+  block.style.backgroundColor = color.value;
+  block.style.zIndex = zIndex.value;
 }
 
 function onClick2() {
+  let zIndex = document.getElementById("z-index2");
+  let color = document.getElementById("color2");
   let position = document.getElementById("position2");
   let button2 = document.getElementById("button2");
   let block2 = document.getElementById("box");
@@ -28,6 +34,8 @@ function onClick2() {
   block2.style.left = inputLeft2.value + unitOfMeasurement2.value;
   block2.style.right = inputRight2.value + unitOfMeasurement2.value;
   block2.style.bottom = inputBottom2.value + unitOfMeasurement2.value;
+  block2.style.backgroundColor = color.value;
+  block2.style.zIndex = zIndex.value;
 }
 
 function openDialog(event) {
@@ -50,3 +58,13 @@ function closeDialog2(event) {
   event.preventDefault();
   document.getElementById("2").close();
 }
+
+document.addEventListener("keydown", (event) => {
+  const key = event.key;
+  if (key === "1") {
+    openDialog(event);
+  }
+  if (key === "2") {
+    openDialog2(event);
+  }
+});
