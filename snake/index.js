c96cc3cb8a36965d3e83805e2249b0ded55dbfc0
Azamat Nigmatullin <ngmazamat@gmail.com>
2024-12-23 16:38:16 +0400

    commit

---
+++ snake/index.js
@@ -0,0 +1,37 @@
+function onClick() {
+  const block = document.createElement("div");
+  block.id = "block";
+  block.body.append();
+  let zIndex = document.getElementById("z-index");
+  let color = document.getElementById("color");
+  let position = document.getElementById("position");
+  let inputTop = document.getElementById("top");
+  let inputLeft = document.getElementById("left");
+  let inputRight = document.getElementById("right");
+  let unitOfMeasurement = document.getElementById("select");
+  let inputBottom = document.getElementById("bottom");
+  block.style.position = position.value;
+  block.style.top = inputTop.value + unitOfMeasurement.value;
+  block.style.left = inputLeft.value + unitOfMeasurement.value;
+  block.style.right = inputRight.value + unitOfMeasurement.value;
+  block.style.bottom = inputBottom.value + unitOfMeasurement.value;
+  block.style.backgroundColor = color.value;
+  block.style.zIndex = zIndex.value;
+}
+
+function openDialog(event) {
+  event.preventDefault();
+
+  document.getElementById("1").showModal();
+}
+
+function closeDialog(event) {
+  event.preventDefault();
+  document.getElementById("1").close();
+}
+document.addEventListener("keydown", (event) => {
+  const key = event.key;
+  if (key === "1") {
+    openDialog(event);
+  }
+});
