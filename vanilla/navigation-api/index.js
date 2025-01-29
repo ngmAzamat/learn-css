36783ffe22da1ae03311638bfd474f6cc1d36d60
Azamat Nigmatullin <ngmazamat@gmail.com>
2025-01-29 11:22:43 +0400

    Navigation Api

--- vanilla/navigation-api/index.js
+++ vanilla/navigation-api/index.js
@@ -1,11 +1,13 @@
+if ("navigation" in window && "intercept" in Navigation.prototype) {
+  console.log("Navigation API поддерживается!");
+} else {
+  console.log("Navigation API НЕ поддерживается в этом браузере.");
+}
+
 navigation.addEventListener("navigate", (navigateEvent) => {
   if (shouldNotIntercept(navigateEvent)) return;
   const url = new URL(navigateEvent.destination.url);
-  if ("navigation" in window && "intercept" in Navigation.prototype) {
-    console.log("Navigation API поддерживается!");
-  } else {
-    console.log("Navigation API НЕ поддерживается в этом браузере.");
-  }
+
   if (url.pathname.startsWith("/navigation-api/about")) {
     navigateEvent.intercept({
       async handler() {
